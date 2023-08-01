02269ac7e2eb093780f475ca4fdb83fa55bef54c
lovleenkaur <lovleenk99@gmail.com>
2023-08-01 16:44:52 -0400

    lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,29 @@
 import React from "react";
+import {useState} from "react";
+
+
 
 function Item({ name, category }) {
+  
+  // const [items, setItems] = useState(0)
+  const [cart, setCart] =  useState(false)
+  
+  function cartButton () { 
+    
+    if (cart === true) { 
+      setCart(false)
+    } else { 
+      setCart(true)
+    }
+  }
   return (
-    <li className="">
+    <li className= {cart ? "in-cart" : "" }>
       <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <span className="category" >{category}</span>
+      <button onClick={cartButton} 
+      className={cart? "remove" : "add"} 
+        
+        >{cart ? "Remove from Cart" : "Add To Cart"}</button>
     </li>
   );
 }
